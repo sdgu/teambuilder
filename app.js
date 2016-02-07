574db076dc99ac32ef8d421c1dc31ec9f31987b9
Shawn <sdg18@duke.edu>
2016-02-07 04:05:03 -0500

    use files from showdown, paths work finally

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var moves = require("./public/data/moves");
+
+
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+var moves = require("../public/data/moves");
+
+var moveList = [];
+for (var move in moves.BattleMovedex)
+{
+	console.log(move);
+	moveList.push(moves.BattleMovedex[move]['name']);
+}
+console.log(moveList);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Teambuilder" });
