609f5bcbadebe9987b7b851a1f5423bd811d1678
Shawn <sdgu000@gmail.com>
2016-02-07 20:51:59 -0500

    autocompleting moves as p instead of dropdown

--- public/javascripts/teambuilder.js
+++ public/javascripts/teambuilder.js
@@ -1,12 +1,110 @@
 var app = angular.module("teambuilder", ["ui.router", "ngSanitize", "ngMaterial"]);
 
+
+// app.factory("movedex", function($http)
+// {
+// 	var o = 
+// 	{
+// 		movedex: []
+// 	}
+
+// 	o.getAll = function()
+// 	{
+	    
+// 	    return $http.get("/movelist").success(function(data)
+// 	    {
+// 	      //alert("getAllInReturn");
+// 	      angular.copy(data, o.movedex);
+// 	    });
+// 	}
+//   	return o;
+// });
+
+app.service("dex", function()
+{
+	var dex = [];
+
+	this.getAll = function(url)
+	{
+		$.ajax(
+		{
+			url: url,
+			async: false,
+			success: function(data)
+			{
+				dex = data;
+			}
+		});
+		return dex;
+	}
+
+	//the word you are looking for, the array aka the dex
+	this.findRel = function(query, source)
+	{
+		var relevant = "<table>";
+
+		for (var i = 0; i < source.length; i++)
+		{
+			if (source[i].id.contains(query) || )
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		var relevant += "</table>";
+	}
+
+})
+
+
 app.controller("MainCtrl", 
 [
 	"$scope",
-	function($scope)
+	"dex",
+	function($scope, dex)
 	{
 		var firstMove = $scope.move1;
 		$scope.move1 = firstMove;
+
+		var movelist = dex.getAll("/movelist");
+
+		$scope.findRelMoves = function(e)
+		{
+			var q = $scope.move1;
+			var relevantMoves = "<table>";
+			for (var i = 0; i < movelist.length; i++)
+			{
+
+				if (movelist[i].id.contains(q) || movelist[i].name.toLowerCase().contains(q))
+				{
+					relevantMoves += '<tr>';
+
+					relevantMoves += '<td>';
+					relevantMoves += movelist[i].name;
+					relevantMoves += '</td>';
+
+					relevantMoves += '</tr>';
+
+
+
+
+					
+				}
+			}
+			relevantMoves += "</table>";
+			$scope.relMoves = relevantMoves;
+
+		}
+
+
 	}
 ]);
 
--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,24 @@ var router = express.Router();
 
 var moves = require("../public/data/moves");
 
-var moveList = [];
-for (var move in moves.BattleMovedex)
-{
-	console.log(move);
-	moveList.push(moves.BattleMovedex[move]['name']);
-}
-console.log(moveList);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Teambuilder" });
 });
 
+router.get("/movelist", function(req, res, next)
+{
+	var moveList = [];
+	for (var move in moves.BattleMovedex)
+	{
+		//console.log(move);
+		moveList.push(moves.BattleMovedex[move]);
+	}
+	//console.log(moveList);
+	res.send(moveList);
+});
 
 
 module.exports = router;
